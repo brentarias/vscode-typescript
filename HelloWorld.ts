0b56121e9ed1f162ae8ac92527fa46e958262770
Brent Anduaga-Arias <Brent Anduaga-Arias>
2016-02-12 13:56:47 -0700

    Baseline Typescript coniguration

---
+++ HelloWorld.ts
@@ -0,0 +1,10 @@
+class Startup {
+    public static main(): number {
+        console.log('Hello World');
+        return 0;
+    }
+}
+
+//console.log("hey there");
+var stuff = new Startup();
+Startup.main(); 
